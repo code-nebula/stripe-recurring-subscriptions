bf36a0a73e65314fbe2466c997f60c9d528714aa
Amy Sitwala <asitwala17@gmail.com>
2019-02-20 20:27:28 -0600

    Adding 'client' side for tutorial

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const app = express();
 const port = 3000;
 const router = express.Router();
 
-
 var stripe = require('stripe')(process.env.STRIPE_API_KEY);
 
 /* Set up Express to serve HTML files using "res.render" with help of Nunjucks */
@@ -35,7 +34,6 @@ router.get('/', (req, res) => {
     });
 
     res.render('adminView.html', {products: products});
-
   });
 });
 
@@ -76,6 +74,41 @@ router.post('/createPlanForReal', (req, res) => {
       res.render('createPlan.html', {success: true});
     }
   });
+});
+
+router.get('/customerView', (req, res) => {
+  Promise.all([stripe.products.list({}), stripe.plans.list({})]).then((stripeData) => {
+    var products = stripeData[0].data;
+    var plans = stripeData[1].data;
+
+    products.forEach(product => {
+      var filteredPlans = plans.filter((plan) => {
+        return plan.product === product.id;
+      });
+      product.plans = filteredPlans;
+    });
+
+    products = products.filter(product => {
+      return product.plans.length > 0;
+    })
+
+    res.render('customerView.html', {products: products});
+  });
+});
+
+
+router.post('/signUp', (req, res) => {
+  var product = {
+    id: req.body.productId
+  };
+
+  var plan = {
+    id: req.body.planId,
+    name: req.body.planName,
+    amount: req.body.planAmount
+  }
+
+  res.render('signUp.html', {product: product, plan: plan});
 });
 
 
